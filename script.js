93c7279b47dd0a16bbdc5321071ba368d5da0916
t4sn33m-s4h4t <mdsahat6397@gmail.com>
2023-04-29 23:09:58 +0600

    first commit

--- script.js
+++ script.js
@@ -145,7 +145,7 @@ window.addEventListener("load", function () {
     const loader = document.getElementById("loader");
     setTimeout(function () {
         loader.style.display = "none";
-    }, 8000);
+    }, 4000);
 });
 generateGame()
 attachEventListeners()
